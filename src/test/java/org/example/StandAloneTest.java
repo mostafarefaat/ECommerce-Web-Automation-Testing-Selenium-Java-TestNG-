662852ae92eebab8319c61923b7e14a1ba98d699
mostafarefaat <mostafarefaat456@gmail.com>
2025-10-26 12:14:15 +0300

    First E2E Testing Flow

---
+++ src/test/java/org/example/StandAloneTest.java
@@ -0,0 +1,97 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v139.network.Network;
+import org.openqa.selenium.devtools.v139.network.model.ConnectionType;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
+
+public class StandAloneTest {
+
+    public static void main(String[] args){
+
+        WebDriverManager.chromedriver().setup();
+
+        //Set Chrome Options
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+
+        ChromeDriver driver = new ChromeDriver(options);
+
+        //Access DevTools
+        DevTools devTools = driver.getDevTools();
+        devTools.createSession();
+
+        devTools.send(Network.emulateNetworkConditions(
+                false,      // Not Offline
+                500,              //  Latency in ms
+                25000,              // Download Throughput (Bytes/sec)
+                15000,              // Upload Throughput (Bytes/sec)
+                Optional.of(ConnectionType.CELLULAR2G), // Correct Enum
+                Optional.empty(),           // Packet Loss
+                Optional.empty(),           // PacketQueueLength
+                Optional.empty()            // PacketReordering
+
+        ));
+
+
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        driver.get("https://rahulshettyacademy.com/client");
+        driver.findElement(By.id("userEmail")).sendKeys("SaA@gmail.com");
+        driver.findElement(By.id("userPassword")).sendKeys("Sa@123456");
+        driver.findElement(By.id("login")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[aria-label*='Login Successfully']")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
+
+        String productName = "ZARA COAT 3";
+        List<WebElement> homeProducts = driver.findElements(By.cssSelector(".mb-3"));
+        WebElement homeProduct = homeProducts.stream().
+                filter(product->product.findElement(By.cssSelector("b")).getText().equals(productName))
+                .findFirst().orElse(null);
+
+        assert homeProduct != null;
+
+        homeProduct.findElement(By.xpath("//button[2]")).click();
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("ngx-spinner.ng-tns-c31-1")));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("toast-container"))));
+
+        driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+
+        List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3"));
+
+        boolean match = cartProducts.stream().anyMatch(cartProduct-> cartProduct.getText().equalsIgnoreCase(productName));
+
+        Assert.assertTrue(match);
+
+        driver.findElement(By.xpath("//button[text()='Checkout']")).click();
+
+        Actions action = new Actions(driver);
+
+        action.sendKeys(driver.findElement(By.cssSelector("input[placeholder='Select Country']")),"Egypt").build().perform();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("ta-results")));
+
+        driver.findElement(By.xpath("//section/button/span[text()=' Egypt']")).click();
+
+        driver.findElement(By.className("action__submit")).click();
+
+        String confirmMsg = driver.findElement(By.className("hero-primary")).getText();
+        Assert.assertEquals(confirmMsg,"THANKYOU FOR THE ORDER");
+
+    }
+}
