8e8698b0c5bd1f7d82e12ff1a302bb65d89c8a87
mostafarefaat <mostafarefaat456@gmail.com>
2026-01-02 11:01:27 +0200

    Modifying the xml file and adding groups for the The Test

--- src/test/java/org/Tests/LoginErrorValidationTest.java
+++ src/test/java/org/Tests/LoginErrorValidationTest.java
@@ -8,14 +8,15 @@ import java.io.IOException;
 
 public class LoginErrorValidationTest extends BaseTest {
 
-    @Test
+
+    @Test (groups = {"ErrorHandling"})
     public void testInvalidEmail_ShowsErrorMessage() throws IOException{
 
         landingPage.loginApplication("Wrong@gmail.com","Sa@123456");
         Assert.assertEquals(landingPage.getErrorMsg(),"Incorrect email or password.");
     }
 
-    @Test
+    @Test (groups = {"ErrorHandling"})
     public void testInvalidPassword_ShowsErrorMessage() throws IOException {
 
         landingPage.loginApplication("SaA@gmail.com","Sa@12345678");
