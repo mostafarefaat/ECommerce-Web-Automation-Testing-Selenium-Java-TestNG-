266f6d7a4f431e372d69d5128bff0ad72af23867
mostafarefaat <mostafarefaat456@gmail.com>
2026-01-08 20:15:11 +0200

    1 - Making the test Run Parallel 2 - Adding Retry Mechanism" 3 - Handling Concurrent=cy Issue for Listeners for reports

---
+++ src/test/java/org/TestComponents/Retry.java
@@ -0,0 +1,18 @@
+package org.TestComponents;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class Retry implements IRetryAnalyzer {
+
+    int count = 0;
+    int maxRetry = 1;
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        if(count<maxRetry){
+            count++;
+            return true;
+        }
+        return false;
+    }
+}
--- src/test/java/org/Tests/LoginErrorValidationTest.java
+++ src/test/java/org/Tests/LoginErrorValidationTest.java
@@ -1,6 +1,7 @@
 package org.Tests;
 
 import org.TestComponents.BaseTest;
+import org.TestComponents.Retry;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,14 +10,14 @@ import java.io.IOException;
 public class LoginErrorValidationTest extends BaseTest {
 
 
-    @Test (groups = {"ErrorHandling"})
+    @Test (groups = {"ErrorHandling"}, retryAnalyzer = Retry.class)
     public void testInvalidEmail_ShowsErrorMessage() throws IOException{
 
         landingPage.loginApplication("Wrong@gmail.com","Sa@123456");
         Assert.assertEquals(landingPage.getErrorMsg(),"Incorrect email or password.");
     }
 
-    @Test (groups = {"ErrorHandling"})
+    @Test (groups = {"ErrorHandling"}, retryAnalyzer = Retry.class)
     public void testInvalidPassword_ShowsErrorMessage() throws IOException {
 
         landingPage.loginApplication("SaA@gmail.com","Sa@12345678");
